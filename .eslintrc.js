5345899271cf70b783cfc12e121cf1130695c3a7
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-02-26 20:49:59 +0300

    add eslint, gitignore, jest

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    browser: true,
+    node: true,
+  },
+  globals: {
+    document: false,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'no-plusplus': 'off',
+    'eslint-disable no-undef': 'off',
+  },
+};
