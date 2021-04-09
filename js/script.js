36b8ce5f4d8358c1f0364b5652e8918e776c223b
VladimirSh6892 <vladimirsh6892@gmail.com>
2021-04-09 21:12:07 +0300

    add method visible privat, corect writeYG,func to method

--- js/script.js
+++ js/script.js
@@ -1,67 +1,90 @@
 "use strict";
 
-// first
-
-let numberOfFilms;
-
-function start () {
-  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
-  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
-  }
-}
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
-  genres: {},
-  private: false
-};
+  genres: [],
+  private: false,
+  start: function () {
+    personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt('Один и зпоследних просмотреных фильмов?', ''),
-      b = prompt('На сколько оцените его?', '');
-  
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-      personalMovieDB.movies[a] = b;
-      console.log('done')
-    } else {
-      console.log('error');
-      i--;
+    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+      personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?');
     }
-  }
+  },
+
+  rememberMyFilms: function () {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt('Один и зпоследних просмотреных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+      } else {
+        console.log('error');
+        i--;
+      }
+    }
+  },
+
+  detectPersonalLevel: function() {
+
+    if (personalMovieDB.count < 10) {
+      console.log('Просмотрено довольно мало фильмов');
+
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 38) {
+      console.log('Вы класический зритель');
+
+    } else if (personalMovieDB.count >= 30) {
+      console.log('Вы киноман');
+
+    } else {
+      console.log('Произошла ошибка');
+    }
+  },
+
+  showMyDB: function (hidden) {
+
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },
+
+  toggleVisibleMyDB: function () {
+      if (personalMovieDB.private) {
+          personalMovieDB.private = false;
+      } else{
+          personalMovieDB.private = true;
+      }
+  },
+
+  writeYourGenres: function () {
+    for (let i = 1; i < 2; i++) {
+
+      let genres = prompt(`Введите ваши любимые жанры через запятую`).toLowerCase();
+
+      if (genres === '' || genres == null) {
+          console.log('Вы ввели некорректные данные или не ввели их вовсе');
+        i--;
+      } else {
+        personalMovieDB.genres = genres.split(', ');
+        personalMovieDB.genres.sort();
+      } 
+
+      //  let genre= prompt(`Ваш любимый жанр под номером ${i}`);
+
+      //  if (genre === '' || genre == null) {
+      //  console.log('Вы ввели некоректные данные или не ввели вовсе');
+      //  i--;
+      //  } else {
+      //  personalMovieDB.genres[i - 1] = genre
+      //  }
+    }
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`Любимый жанр ${i + 1} - это ${item}`);
+    });
+  },
 };
 
-rememberMyFilms();
-
-function detectPersonalLevel() {
-  if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 38) {
-    console.log('Вы класический зритель');
-  } else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-  } else {
-    console.log('Произошла ошибка');
-  }
-}
-
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
-
-showMyDB(personalMovieDB.private);
-
-function writeYourGenres() {
-  for (let i = 1; i <= 3; i++) {
-    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${1}`);
-  }
-};
-
-writeYourGenres();
